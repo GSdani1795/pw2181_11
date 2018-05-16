8dc8b15a9f0a4d0bf05e0d4deca5ea4190b8b39d
ALAPIZCO ACOSTA JESHUA GILDARDO <13170210@itculiacan.edu.mx>
2018-05-16 11:57:28 -0600

    PHP

---
+++ primera.php
@@ -0,0 +1,22 @@
+<?php  
+$parametro1 = %_GET["parametro1"];
+$variable = 0;
+$otra = "";
+$otradeotra = 0.0;
+
+for ($i=0; $i < 10 ; $i++) { 
+//echo
+print("Text".$i."<br>");
+
+}
+?>
+<html>
+<head>
+	<title> </title>
+</head>
+<body>
+Hola mundo <?php print($parametro);   ?>
+</body>
+</html>
+
+
